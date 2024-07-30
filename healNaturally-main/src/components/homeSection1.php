984c2e4c5d2c00de3cc406ea89bfbbd56dbe2643
Atiq80 <work.special@outlook.com>
2024-07-30 20:50:29 +0500

    Add files via upload

---
+++ healNaturally-main/src/components/homeSection1.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mini about section - Home section 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    
+      <!-- Home section 1 - mini about section on Home page -->
+       <section class="container mini-about-section">
+        <div class="mini-about-heading-div">
+            
+        <h1 class="mini-about-heading">Embrace wellness with Heal<span class="change-color">Naturally</span></h1>
+        </div>
+
+        <div>
+            <p class="mini-about-para-1">At HealNaturally, we believe in the power of nature and holistic wellness to achieve a healthier, happier life. Our web app is your go-to resource for natural remedies, exercise guidance, dietary tips, and hydration reminders, all designed to help you thrive.</p>
+        </div>
+
+        <div class="row py-3 d-flex mini-about-row">
+            <div class="col col-sm-12 col-md-6 col-lg-6 mini-about-text">
+                <h3 class="offer-heading">What we offer</h3>
+                <ul>
+                    <li><h6>Local Remedies:</h6></li>
+                    <p>Unlock the healing potential of nature with our curated list of local remedies for common ailments. Discover time-tested solutions that are both effective and easy to implement.</p>
+
+                    <li><h6>Exercise Tips:</h6></li>
+                    <p>Stay active and fit with our daily exercise recommendations. Whether you're a beginner or a fitness enthusiast, our tips are tailored to suit all levels and help you achieve your wellness goals.</p>
+
+                    <li><h6>Dietary Tips:</h6></li>
+                    <p>Nourish your body with personalized dietary advice. Our tips are designed to help you make healthier food choices, improve your eating habits, and achieve a balanced diet.</p>
+
+                    <li><h6>Hydration Reminders:</h6></li>
+                    <p>Never miss a sip with our smart hydration reminders. Staying hydrated is key to overall health, and we're here to ensure you get enough water throughout the day.</p>
+                </ul>
+            </div>
+            <div class="col col-sm-12 col-md-6 col-lg-5 mini-about-imgs">
+                    <img src="../assets/images/herbal-mash.png" alt="herb-img" class="herb-img">
+                    <img src="../assets/images/Running-pana.svg" alt="jogger" class="jogger">
+            </div>
+        </div>
+
+       </section>
+      <!-- Home section 1 - mini about section on Home page -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
