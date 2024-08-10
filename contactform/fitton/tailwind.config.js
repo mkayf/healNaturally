5b11fecdcab2061176de0fa33318d16d1c1b9320
Atiq80 <work.special@outlook.com>
2024-08-10 13:34:03 +0500

    Contact form

---
+++ contactform/fitton/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+    },
+    },
+  },
+  plugins: [],
+}
+
